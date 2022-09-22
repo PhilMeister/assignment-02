3d6771af00511b47a1815acecdde77df65aeb863
PhilMeister <45036526+PhilMeister@users.noreply.github.com>
2022-09-22 13:45:47 +0200

    wizards done sln fix

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,4 +1,8 @@
-﻿foreach (var wizard in WizardCollection.Create())
+﻿/* foreach (var wizard in WizardCollection.Create())
 {
     Console.WriteLine(wizard);
 }
+ */
+ Console.WriteLine("hej");
+ var res = new Queries();
+ res.query1em();
--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -2,4 +2,14 @@ namespace Assignment2;
 
 public class Queries
 {
+
+    public string query1em()
+    {
+        var wizardlst = WizardCollection.Create();
+        var sithL = wizardlst.Where(wizard => wizard.Name.Contains("Darth"));
+
+
+        sithL.ToList().ForEach(wizard => Console.WriteLine(wizard.ToString()));
+        return "hej";
+    }
 }
